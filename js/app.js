ea8bb420ec2534fa4b44c6bb86bf5f50a3a48b51
William Markley <willjmarkley@gmail.com>
2017-03-03 15:23:24 -0500

    updated mongo db

--- js/app.js
+++ js/app.js
@@ -8,29 +8,47 @@ const mongo = require('mongodb').MongoClient
 
 // global variables
 const port = 8080;
-const databaseURL = 'mongodb://localhost:27017/nodemongo';
+const databaseURL = 'mongodb://localhost:27017/irishlink';
 
 
 // database functions
-function readAll(db, collectionName, response) {
-        var collection = db.collection(collectionName);
-        collection.find({}, { _id: 0 }).toArray( function(err, allData) {
-                assert.equal(err, null);
-                console.log("Sent JSON data");
-                response.json(allData[0]);
-        });
+function retrieveAll(db, collectionName, response) {
+	var collection = db.collection(collectionName);
+	collection.find({}, { _id: 0 }).toArray( function(err, allData) {
+			assert.equal(err, null);
+			console.log("Successfully Sent JSON data");
+			response.json(allData[0]);
+	});
 }
 
 
 
 // routing
+app.get('/ideas', function (req, res) {
+	mongo.connect(databaseURL, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to mongo server");
+		retrieveAll(db, 'ideas', res);
+		db.close();
+	});
+})
+
+app.get('/developers', function (req, res) {
+	mongo.connect(databaseURL, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected successfully to mongo server");
+		retrieveAll(db, 'developers', res);
+		db.close();
+	});
+})
+
 app.get('/', function (req, res) {
 	mongo.connect(databaseURL, function(err, db) {
-                assert.equal(null, err);
-                console.log("Connected successfully to mongo server");
-                readAll(db, 'test', res);
-                db.close();
-        });
+		assert.equal(null, err);
+		console.log("Connected successfully to mongo server");
+		retrieveAll(db, 'test', res);
+		db.close();
+	});
 })
 
 
