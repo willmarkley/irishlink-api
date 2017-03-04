e14ce017a6c71f32007706eb603fa7a5b27673fd
Will Markley <willjmarkley@gmail.com>
2017-03-04 18:11:24 +0000

    Fixed POST requests

--- js/app.js
+++ js/app.js
@@ -19,17 +19,18 @@ function retrieveAll(db, collectionName, response) {
 	collection.find({}, { _id: 0 }).toArray( function(err, allData) {
 			assert.equal(err, null);
 			console.log("Successfully Sent JSON data");
-			response.json(allData[0]);
+			response.json(allData);
 	});
 }
 
 function addEntry(db, collectionName, data, response) {
 	var collection = db.collection(collectionName);
-	if !Object.keys(data).length {  // json  data is empty
+	if (!Object.keys(data).length) {  // json  data is empty
 		console.log("JSON data is empty");
 		response.send("JSON data is empty");
 	}
 	else {
+		console.log(data);
 		collection.insertOne(data, function(err, result) {
 			assert.equal(err, null);
 			assert.equal(1, result.insertedCount);
@@ -42,6 +43,8 @@ function addEntry(db, collectionName, data, response) {
 
 
 // routing
+app.use(bodyParser.json());
+
 app.get('/ideas', function (req, res) {
 	mongo.connect(databaseURL, function(err, db) {
 		assert.equal(null, err);
@@ -85,9 +88,8 @@ app.get('/*', function (req, res) {
 
 
 
-// prettify JSON and set body parser
+// prettify JSON
 app.set('json spaces', 4);
-app.use(bodyParser.json());
 
 // listen on port
 app.listen(port, function () {
