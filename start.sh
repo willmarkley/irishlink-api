5e3f3579603b63ab69876ed592a3429b628c2393
Will Markley <willmarkley@users.noreply.github.com>
2017-08-27 16:00:09 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-sudo /usr/local/bin/mongod > /dev/null &
-#node js/app.js > console.log &
-pm2 restart all
-sudo service nginx restart
+
+if [ "$1" = "init" ]; then
+    sudo /usr/local/bin/mongod > /dev/null &
+    #node js/app.js > console.log &
+    pm2 start js/app.js
+    sudo service nginx start
+elif [ "$1" = "stop" ]; then
+    sudo /usr/local/bin/mongod --shutdown
+    pm2 delete all
+    sudo service nginx stop
+else
+    sudo /usr/local/bin/mongod > /dev/null &
+    pm2 restart all
+    sudo service nginx restart
